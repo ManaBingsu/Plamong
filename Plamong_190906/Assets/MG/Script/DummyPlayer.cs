52cf06162d33d27a570829261dfb3399f2c769ea
kmko0914 <kmko0914@gmail.com>
2019-10-12 16:57:46 +0900

    [MG setting] .

---
+++ Plamong_190906/Assets/MG/Script/DummyPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyPlayer : MonoBehaviour
+{
+    public static DummyPlayer player;
+
+    // 플레이어 데이터
+    public PlayerData playerData;
+
+    private void Awake()
+    {
+        if (player == null)
+            player = this;
+    }
+}
